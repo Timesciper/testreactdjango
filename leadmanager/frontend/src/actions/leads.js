bc87397f655558d0b6255b5a1f9d16c88c343477
Alexandr Chabakauri <timesciper@live.ru>
2019-09-12 16:44:17 +0300

    redux get lead and delete lead

---
+++ leadmanager/frontend/src/actions/leads.js
@@ -0,0 +1,20 @@
+import {GET_LEADS, DELETE_LEAD} from "./types";
+import axios from 'axios';
+
+export const getLeads = () => dispatch => {
+    axios.get('/api/leads/').then(res => {
+        dispatch({
+            type: GET_LEADS,
+            payload: res.data
+        })
+    }).catch(err => console.log(err));
+};
+
+export const deleteLead = (id) => dispatch => {
+    axios.delete('/api/leads/'+id).then(res => {
+        dispatch({
+            type: DELETE_LEAD,
+            payload: id
+        })
+    }).catch(err => console.log(err));
+};
--- leadmanager/frontend/src/components/leads/Leads.js
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -1,9 +1,55 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {getLeads, deleteLead} from '../../actions/leads';
+
+
 
 export class Leads extends Component{
+    static propTypes ={
+        leads: PropTypes.array.isRequired
+    };
+
+    componentDidMount(){
+        this.props.getLeads();
+    }
     render() {
-        return <h1>LeadsList</h1>
+        return (
+            <Fragment>
+                <h2>Leads</h2>
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Message</th>
+                    <th>Email</th>
+                    <th>Comment</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    { this.props.leads.map(lead => (
+                        <tr key={lead.id}>
+                            <td>{lead.id}</td>
+                            <td>{lead.name}</td>
+                            <td>{lead.message}</td>
+                            <td>{lead.email}</td>
+                            <td>{lead.comment}</td>
+                            <td><button className='btn btn-danger' onClick={this.props.deleteLead.bind(this, lead.id)}>{" "} Delete </button></td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </Fragment>
+        )
     }
 }
 
-export default Leads
+
+const mapStateToProps = state => ({
+    leads: state.leads.leads //state.leadReducer.leads <- leads is part of state
+});
+
+
+
+export default connect(mapStateToProps, {getLeads, deleteLead})(Leads);
